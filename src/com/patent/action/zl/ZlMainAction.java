613386eb687a1c8cc30d3c16f785cf391bfbe872
sandywm <270826057@qq.com>
2018-11-15 16:25:26 +0800

    提交 Signed-off-by: sandywm <270826057@qq.com>

--- src/com/patent/action/zl/ZlMainAction.java
+++ src/com/patent/action/zl/ZlMainAction.java
@@ -1242,25 +1242,27 @@ public class ZlMainAction extends DispatchAction {
 					String upFile_db = mx.getLcMxUpFile();
 					String upFileName = "";//文件名称
 					String upFileSize = "";//文件大小
-					String[] upFileArr = upFile_db.split(",");
-					Integer fileNum = upFileArr.length;
-					Integer upUserId = mx.getLcMxUpUserId();
-					String upUserName = "";
-					if(upUserId > 0){
-						upUserName = cum.getEntityById(mx.getLcMxUpUserId()).getUserName();//上传人
-					}
-					String upDate = mx.getLcMxUpDate();//上传日期
 					List<Object> list_mx_1 = new ArrayList<Object>();
-					for(Integer i = 0 ; i < fileNum ; i++){
-						upFileName = upFileArr[i].substring(upFileArr[i].lastIndexOf("\\")+1,upFileArr[i].length());
-						upFileSize = FileOpration.getFileSize(WebUrl.DATA_URL_UP_FILE_UPLOAD + "\\" + upFileArr[i]);
-						Map<String,String> map_mx = new HashMap<String,String>();
-						map_mx.put("upFileName", upFileName);
-						map_mx.put("upUserName", upUserName);
-						map_mx.put("upDate", upDate);
-						map_mx.put("upFileSize", upFileSize);
-						map_mx.put("downFilePath", upFileArr[i]);
-						list_mx_1.add(map_mx);
+					if(!upFile_db.equals("")){//存在附件
+						String[] upFileArr = upFile_db.split(",");
+						Integer fileNum = upFileArr.length;
+						Integer upUserId = mx.getLcMxUpUserId();
+						String upUserName = "";
+						if(upUserId > 0){
+							upUserName = cum.getEntityById(mx.getLcMxUpUserId()).getUserName();//上传人
+						}
+						String upDate = mx.getLcMxUpDate();//上传日期
+						for(Integer i = 0 ; i < fileNum ; i++){
+							upFileName = upFileArr[i].substring(upFileArr[i].lastIndexOf("\\")+1,upFileArr[i].length());
+							upFileSize = FileOpration.getFileSize(WebUrl.DATA_URL_UP_FILE_UPLOAD + "\\" + upFileArr[i]);
+							Map<String,String> map_mx = new HashMap<String,String>();
+							map_mx.put("upFileName", upFileName);
+							map_mx.put("upUserName", upUserName);
+							map_mx.put("upDate", upDate);
+							map_mx.put("upFileSize", upFileSize);
+							map_mx.put("downFilePath", upFileArr[i]);
+							list_mx_1.add(map_mx);
+						}
 					}
 					map_d.put("upFileDetail", list_mx_1);//附件明细（当上传文件存在时出现）
 					map_d.put("mxRemark", mx.getLcMxRemark());
